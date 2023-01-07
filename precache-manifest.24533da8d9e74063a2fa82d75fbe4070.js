8621e199553f473c1e35dbf704213d8f5a9d96f4
JoselynDRF <mariajoselyndosramos@yahoo.es>
2023-01-07 14:51:26 +0000

    Deploy App to GitHub Pages

---
+++ precache-manifest.24533da8d9e74063a2fa82d75fbe4070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cabc329dabf5f87caa54c577c18791f",
+    "url": "/react-redux-typescript-todolist/index.html"
+  },
+  {
+    "revision": "36353b5f4a8b134ee43c",
+    "url": "/react-redux-typescript-todolist/static/css/main.9f855c79.chunk.css"
+  },
+  {
+    "revision": "518f3236601c6f6fd18e",
+    "url": "/react-redux-typescript-todolist/static/js/2.09bccbab.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/react-redux-typescript-todolist/static/js/2.09bccbab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36353b5f4a8b134ee43c",
+    "url": "/react-redux-typescript-todolist/static/js/main.7a362753.chunk.js"
+  },
+  {
+    "revision": "f95625954fe11a10de68",
+    "url": "/react-redux-typescript-todolist/static/js/runtime-main.db16f1ff.js"
+  }
+]);
